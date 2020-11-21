5adf0a9c4b56b74a27991d8d6b07aef33fb372d2
Pepivsky <ironglas68@gmail.com>
2020-11-21 02:11:48 -0600

    Agregadas clases del proyecto Contacts

---
+++ src/com/company/contacts/Contacts.java
@@ -0,0 +1,85 @@
+package com.company.contacts;
+/*
+Stage 1/4: First contact
+Description
+Contacts is a handy app to have. It stores all of your saved contacts. In this project, you will write one yourself. It teaches you to understand and implement the basic principles of OOP.
+
+In the first stage, you should write a program that creates an instance of a class that stores information about one record in the Contacts. One record should contain a name, a surname, and a phone number. You can type them from the keyboard.
+
+You should also create a class representing this app. For now, it should store only one record: a record that you created from the input.
+
+Output example
+Below is an example of how your output might look. Lines which start with > represent the user input.
+
+Enter the name of the person:
+> John
+Enter the surname of the person:
+> Smith
+Enter the number:
+> 1-234-567-890
+
+A record created!
+A Phone Book with a single record created!
+ */
+
+import java.util.Scanner;
+
+class Contacts {
+    private String name;
+    private String surname;
+    private String phoneNumber;
+
+    Contacts() {
+
+    }
+    Contacts(String name, String surname, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the name of the person:");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter the surname of the person:");
+        String surname = scanner.nextLine();
+
+        System.out.println("Enter the number:");
+        String phoneNumber = scanner.nextLine();
+
+        Contacts contact1 = new Contacts(name, surname, phoneNumber);
+
+        System.out.println("A record created!");
+        System.out.println("A Phone Book with a single record created!");
+    }
+}
+
---
+++ src/com/company/contacts/Main.java
@@ -0,0 +1,77 @@
+package com.company.contacts;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+/*
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Contacts> contactsArrayList = new ArrayList<>();
+        String menuInput = "";
+
+        Contacts contact;
+        String name = "";
+        String sunName = "";
+        String phoneNumber = "";
+
+        while (!menuInput.equals("exit")) {
+            System.out.println("Enter action (add, remove, edit, count, list, exit): >");
+            menuInput = scanner.next();
+            if (menuInput.equals("add")) {
+                System.out.println("Enter the name: >");
+                name = scanner.next();
+                System.out.println("Enter the surname: >");
+                sunName = scanner.next();
+                System.out.println("Enter the number: >");
+                phoneNumber = scanner.next();
+
+                contact = new Contacts(name, sunName, phoneNumber);
+                contactsArrayList.add(contact);
+                System.out.println(contactsArrayList);
+
+            }
+        }
+
+    }
+}
+
+class Contacts {
+    private String name;
+    private String surname;
+    private String phoneNumber;
+
+    Contacts() {
+
+    }
+    Contacts(String name, String surname, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) { //check number
+        this.phoneNumber = phoneNumber;
+    }
+}*/
